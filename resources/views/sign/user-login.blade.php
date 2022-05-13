db53ced031a28ecb3a581a074fb924ba27e15c37
d15118080 <66905665+d15118080@users.noreply.github.com>
2022-05-13 12:56:44 +0900

    Update user-login.blade.php

--- resources/views/sign/user-login.blade.php
+++ resources/views/sign/user-login.blade.php
@@ -1,34 +1,41 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta name="viewport"
+        content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <title>콘페이 - 로그인</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+        rel="stylesheet">
 
     <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
-<div class="wrap">
-    <div class="sign_w login_top_pd">
-        <div class="sign_header">
-            <h2 class="txs_x" style="margin-bottom: 5%; color: #2E56A1"><strong>내손안의 작은 지갑</strong></h2>
-            <h1 class="txs_xl"><strong>콘 페이</strong></h1>
-            <p>서비스를 위해 로그인이 필요합니다.</p>
-        </div>
+    <div class="wrap">
+        <div class="sign_w login_top_pd">
+            <div class="sign_header">
+                <h2 class="txs_x" style="margin-bottom: 5%; color: #2E56A1"><strong>우리들의 작은 계모임</strong></h2>
+                <h1 class="txs_xl"><strong>콘 페이</strong></h1>
+                <p>서비스를 위해 로그인이 필요합니다.</p>
+                <p style="text-align: center;color: blue;">우리들의 작은 온라인 계모임 콘페이 입니다 <br>콘페이는 계주에게 곗돈을 송금하는 앱 입니다 <br> 곗돈
+                    외의 불법적인 용도로 이용시 법적 대응이 이뤄지며 불이익이 생길수 있습니다 <br></p>
+            </div>
 
-        <div class="login">
+            <div class="login">
                 <fieldset>
                     <legend>로그인</legend>
                     <div class="ipt_section">
-                        <div class="ipt_bx id"><!-- 활성화 시 ip클래스 추가 -->
+                        <div class="ipt_bx id">
+                            <!-- 활성화 시 ip클래스 추가 -->
                             <input id="user_id" type="text" placeholder="아이디">
                         </div>
-                        <div class="ipt_bx password"><!-- 활성화 시 ip클래스 추가 -->
+                        <div class="ipt_bx password">
+                            <!-- 활성화 시 ip클래스 추가 -->
                             <input id="user_password" type="password" placeholder="비밀번호">
                             <a href="none" class="view_pass">텍스트보기</a>
                         </div>
@@ -41,13 +48,14 @@
                         <a href="register" class="btn_join">회원가입</a>
                     </div>
                 </fieldset>
+            </div>
         </div>
     </div>
-</div>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-<script src="//unpkg.com/axios/dist/axios.min.js"></script>
-<script type="module"  src="/js/login.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+    <script src="//unpkg.com/axios/dist/axios.min.js"></script>
+    <script type="module" src="/js/login.js"></script>
 </body>
+
 </html>
